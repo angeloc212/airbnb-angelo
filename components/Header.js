f102fb1facc865483e61915d5bd97e3f5a26da39
Angelo <angeloc.sparkles@gmail.com>
2022-06-08 06:22:37 +0800

    feat: calendar select

--- components/Header.js
+++ components/Header.js
@@ -15,13 +15,22 @@ function Header() {
     const[searchInput, setSearchInput] = useState("");
     const[startDate, setStartDate] = useState(new Date());
     const[endDate, setEndDate] = useState(new Date());
+    const[noOfGuests, setNoOfGuests] = useState(1)
+
+    const handleSelect = (ranges) => {
+
+        setStartDate(ranges.selection.startDate);
+        setEndDate(ranges.selection.startDate);
+    }
 
     const selectionRange = {
         startDate: startDate,
         endDate: endDate,
-        key: 'Selection'
+        key: 'selection'
 
     }
+
+    
 
   return (
     <header className='sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md 
@@ -57,11 +66,23 @@ function Header() {
         </div>
 
         {searchInput && (
-            <div>
-                <DateRangePicker  ranges={[selectionRange]}
+            <div className='flex flex-col col-span-3 mx-auto'>
+                <DateRangePicker  
+                ranges={[selectionRange]}
+                minDate={new Date()}
+                rangeColors={["#FD5B61"]}
+                onChange={handleSelect}
                 
                 />
-
+                <div className='flex items-center border-b mb-4'>
+                    <h2 className='text-2xl flex-grow font-semibold'>Number of Guests</h2>
+                    <UserIcon className='h-5' />
+                    <input value={noOfGuests}
+                    onChange={(e) => setNoOfGuests(e.target.value)}
+                    min={1}
+                     type="number" className='w-12 pl-2 text-lg outline-none text-red-400'></input>
+                </div>
+                
             </div>
         )}
         
